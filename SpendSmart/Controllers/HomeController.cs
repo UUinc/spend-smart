a89a1a10bc5426481078bac4ff9ce5356c341937
Yahya LAZREK <63449913+UUinc@users.noreply.github.com>
2024-11-27 21:41:32 +0100

    Showing the total expense
    Showing the total expense amount using the viewbag

--- SpendSmart/Controllers/HomeController.cs
+++ SpendSmart/Controllers/HomeController.cs
@@ -24,6 +24,11 @@ namespace SpendSmart.Controllers
         public IActionResult Expenses()
         {
             var allExpenses = _context.Expenses.ToList();
+
+            var totalExpenses = allExpenses.Sum(x => x.Price);
+
+            ViewBag.Expenses = totalExpenses;
+
             return View(allExpenses);
         }
 
