c1de23d7147be259d1274bd7c4fdbec74453d457
Yahya LAZREK <63449913+UUinc@users.noreply.github.com>
2024-11-27 21:35:29 +0100

    Editing and Deleting expenses

--- SpendSmart/Controllers/HomeController.cs
+++ SpendSmart/Controllers/HomeController.cs
@@ -23,17 +23,39 @@ namespace SpendSmart.Controllers
 
         public IActionResult Expenses()
         {
+            var allExpenses = _context.Expenses.ToList();
+            return View(allExpenses);
+        }
+
+        public IActionResult CreateEditExpense(int? id)
+        {
+            if (id != null)
+            {
+                var expenseInDb = _context.Expenses.SingleOrDefault(expense => expense.Id == id);
+                return View(expenseInDb);
+            }    
+
             return View();
         }
 
-        public IActionResult CreateEditExpense()
+        public IActionResult DeleteExpense(int id)
         {
-            return View();
+            var expenseInDb = _context.Expenses.SingleOrDefault(expense => expense.Id == id);
+            _context.Expenses.Remove(expenseInDb);
+            _context.SaveChanges();
+            return RedirectToAction("Expenses");
         }
 
         public IActionResult CreateEditExpenseForm(Expense model)
         {
-            _context.Expenses.Add(model);
+            if (model.Id == 0)
+            {
+                _context.Expenses.Add(model);
+            }
+            else
+            {
+                _context.Expenses.Update(model);
+            }
             _context.SaveChanges();
             return RedirectToAction("Expenses");
         }
